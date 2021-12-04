771844d806afe94d56e22ae9fec27ca3e7b3f740
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2021-12-04 11:13:09 -0500

    Local MAIN: create ADN TransHumans

---
+++ contracts/ADN_TransHumans.sol
@@ -0,0 +1,203 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract ADNTransHumans {
+    string[] private _accessoriesType = [
+        "Blank",
+        "Kurt",
+        "Prescription01",
+        "Prescription02",
+        "Round",
+        "Sunglasses",
+        "Wayfarers"
+    ];
+
+    string[] private _clotheColor = [
+        "Black",
+        "Blue01",
+        "Blue02",
+        "Blue03",
+        "Gray01",
+        "Gray02",
+        "Heather",
+        "PastelBlue",
+        "PastelGreen",
+        "PastelOrange",
+        "PastelRed",
+        "PastelYellow",
+        "Pink",
+        "Red",
+        "White"
+    ];
+
+    string[] private _clotheType = [
+        "BlazerShirt",
+        "BlazerSweater",
+        "CollarSweater",
+        "GraphicShirt",
+        "Hoodie",
+        "Overall",
+        "ShirtCrewNeck",
+        "ShirtScoopNeck",
+        "ShirtVNeck"
+    ];
+
+    string[] private _eyeType = [
+        "Close",
+        "Cry",
+        "Default",
+        "Dizzy",
+        "EyeRoll",
+        "Happy",
+        "Hearts",
+        "Side",
+        "Squint",
+        "Surprised",
+        "Wink",
+        "WinkWacky"
+    ];
+
+    string[] private _eyebrowType = [
+        "Angry",
+        "AngryNatural",
+        "Default",
+        "DefaultNatural",
+        "FlatNatural",
+        "RaisedExcited",
+        "RaisedExcitedNatural",
+        "SadConcerned",
+        "SadConcernedNatural",
+        "UnibrowNatural",
+        "UpDown",
+        "UpDownNatural"
+    ];
+
+    string[] private _facialHairColor = [
+        "Auburn",
+        "Black",
+        "Blonde",
+        "BlondeGolden",
+        "Brown",
+        "BrownDark",
+        "Platinum",
+        "Red"
+    ];
+
+    string[] private _facialHairType = [
+        "Blank",
+        "BeardMedium",
+        "BeardLight",
+        "BeardMagestic",
+        "MoustacheFancy",
+        "MoustacheMagnum"
+    ];
+
+    string[] private _hairColor = [
+        "Auburn",
+        "Black",
+        "Blonde",
+        "BlondeGolden",
+        "Brown",
+        "BrownDark",
+        "PastelPink",
+        "Platinum",
+        "Red",
+        "SilverGray"
+    ];
+
+    string[] private _hatColor = [
+        "Black",
+        "Blue01",
+        "Blue02",
+        "Blue03",
+        "Gray01",
+        "Gray02",
+        "Heather",
+        "PastelBlue",
+        "PastelGreen",
+        "PastelOrange",
+        "PastelRed",
+        "PastelYellow",
+        "Pink",
+        "Red",
+        "White"
+    ];
+
+    string[] private _graphicType = [
+        "Bat",
+        "Cumbia",
+        "Deer",
+        "Diamond",
+        "Hola",
+        "Pizza",
+        "Resist",
+        "Selena",
+        "Bear",
+        "SkullOutline",
+        "Skull"
+    ];
+
+    string[] private _mouthType = [
+        "Concerned",
+        "Default",
+        "Disbelief",
+        "Eating",
+        "Grimace",
+        "Sad",
+        "ScreamOpen",
+        "Serious",
+        "Smile",
+        "Tongue",
+        "Twinkle",
+        "Vomit"
+    ];
+
+    string[] private _skinColor = [
+        "Tanned",
+        "Yellow",
+        "Pale",
+        "Light",
+        "Brown",
+        "DarkBrown",
+        "Black"
+    ];
+
+    string[] private _topType = [
+        "NoHair",
+        "Eyepatch",
+        "Hat",
+        "Hijab",
+        "Turban",
+        "WinterHat1",
+        "WinterHat2",
+        "WinterHat3",
+        "WinterHat4",
+        "LongHairBigHair",
+        "LongHairBob",
+        "LongHairBun",
+        "LongHairCurly",
+        "LongHairCurvy",
+        "LongHairDreads",
+        "LongHairFrida",
+        "LongHairFro",
+        "LongHairFroBand",
+        "LongHairNotTooLong",
+        "LongHairShavedSides",
+        "LongHairMiaWallace",
+        "LongHairStraight",
+        "LongHairStraight2",
+        "LongHairStraightStrand",
+        "ShortHairDreads01",
+        "ShortHairDreads02",
+        "ShortHairFrizzle",
+        "ShortHairShaggyMullet",
+        "ShortHairShortCurly",
+        "ShortHairShortFlat",
+        "ShortHairShortRound",
+        "ShortHairShortWaved",
+        "ShortHairSides",
+        "ShortHairTheCaesar",
+        "ShortHairTheCaesarSidePart"
+    ];
+}
